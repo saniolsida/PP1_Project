1860bb0f9e980e85af9deb714cfcf9b4659ebf8d
saniolsida <songsan3133@gmail.com>
2023-09-01 16:51:27 +0900

    Creat Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("219000", "홍길동");
+        System.out.println(s1.toString());
+
+    }
+}
