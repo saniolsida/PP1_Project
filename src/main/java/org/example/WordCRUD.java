7ab3bd829b6d4d4c4a016fa8395c36a315d86be3
saniolsida <songsan3133@gmail.com>
2023-09-05 22:29:27 +0900

    Implement deleteWord function

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -71,5 +71,19 @@ public class WordCRUD implements ICRUD{
     }
     @Override
     public void deleteWord() {
+        System.out.print("=> 삭제할 단어 검색: ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 삭제할 번호 선택: ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("==> 정말로 삭제하시겠습니까?(Y/n)  ");
+        String ans = s.next();
+        if(ans.equalsIgnoreCase("y")){
+            list.remove((int)idlist.get(id-1));
+            System.out.println("단어가 삭제되었습니다. ");
+        }
+        else
+            System.out.println("취소되었습니다. ");
     }
 }
